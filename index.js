9418b2f660c3bd3297b44c378d27380ef7fba0c2
Tiross <Tiross@users.noreply.github.com>
2018-04-14 21:57:37 +0200

    First implementation

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const loop = require('./lib/loop');
+const mqtt = require('./lib/mqtt');
+const config = require('./config.json');
+
+config.verbose = config.verbose || false;
+
+if (undefined === config.sources || !config.sources.length) {
+  if (config.verbose) {
+    console.error('No sources configured. Please configure at least one source and try again.');
+  }
+
+  process.exit(1);
+}
+
+mqtt.connect(config.broker);
+
+config.sources.forEach((source, index) => {
+  if (!source || typeof(source.host) === 'undefined' || !source.host) {
+    if (config.verbose) {
+      console.error('Missing required argument "host"');
+    }
+
+    process.exit(1);
+  }
+
+  source.id = index;
+  source.interval = source.interval || 1000;
+  source.prefix = source.prefix || 'tv';
+  source.data = {
+    status: false,
+    volume: 0,
+    muted: null,
+    source: '',
+  };
+
+  if (config.verbose) {
+    console.log('Polling source "%s" every %dms', source.host, source.interval);
+  }
+
+  loop(source);
+});
